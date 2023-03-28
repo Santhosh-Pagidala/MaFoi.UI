e9e57f2f1d9415e7801edc2b4b550543f27f768a
Santhosh-Pagidala <31021669+Santhosh-Pagidala@users.noreply.github.com>
2023-03-28 13:06:20 +0530

    Adding git ignore, package-lock and few fixes to vendor activities filters

--- src/components/pages/Vendordashboard/vendorActivityToDo.js
+++ src/components/pages/Vendordashboard/vendorActivityToDo.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faPencilSquare } from "@fortawesome/free-solid-svg-icons";
-import { faEye, faFloppyDisk } from "@fortawesome/free-regular-svg-icons";
 import dayjs from "dayjs";
 import BulkUploadModal from "./bulkuploadModal";
 import * as api from "../../../backend/request";
+import Select from 'react-select';
 
 function StatusTmp({ status }) {
   function computeStatusColor(status) {
@@ -29,10 +27,17 @@ export class VendorActivityToDo extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { posts: [], res: [], uploadFiles: [], show: false };
+    this.state = { res: [], show: false };
   }
 
   componentDidMount() {
+    api.get('/api/Company/GetUserCompanies').then(response => {
+      const companies = (response.data || []).map(company => {
+        return { value: company.id, label: company.name, company }
+      });
+      this.setState({ companies });
+      this.onCompanyChange();
+    });
     api.get('/api/ToDo/GetAll').then(response => {
       this.setState({ res: response.data || [] });
     });
@@ -44,8 +49,21 @@ export class VendorActivityToDo extends Component {
   }
 
   handleClose() {
-    this.setState({ uploadFiles: [] });
     this.setState({ show: false });
+  }
+
+  onCompanyChange(event) {
+    this.setState({ associateCompanies: [], locations: [], selectedAssociateCompany: null, selectedLocation: null });
+    this.setState({ selectedCompany: event });
+    if (event && event.value) {
+      const associateCompanies = event.company.associateCompanies.map(associateCompany => {
+        return { label: associateCompany.name, value: associateCompany.id };
+      });
+      const locations = event.company.locations.map(location => {
+        return { label: `${location.name}, ${location.cities.name}`, value: location.id, location };
+      });
+      this.setState({ associateCompanies, locations });
+    }
   }
 
 
@@ -104,28 +122,16 @@ export class VendorActivityToDo extends Component {
           <div className="card-body">
             <div className="row">
               <div className="col-2 col-md-2">
-                <select className="form-select">
-                  <option>Select</option>
-                  <option>Company A</option>
-                  <option>Company A</option>
-                  <option>Company C</option>
-                </select>
+                <Select options={this.state.companies} onChange={this.onCompanyChange.bind(this)}
+                  placeholder='Company' value={this.state.selectedCompany} />
               </div>
               <div className="col-3 col-md-3">
-                <select className="form-select">
-                  <option>Associate Company</option>
-                  <option>Company A</option>
-                  <option>Company A</option>
-                  <option>Company C</option>
-                </select>
+                <Select options={this.state.associateCompanies} onChange={(event) => this.setState({ selectedAssociateCompany: event })}
+                  placeholder='Associate Company' value={this.state.selectedAssociateCompany} />
               </div>
               <div className="col-2 col-md-2">
-                <select className="form-select">
-                  <option>Location</option>
-                  <option>Company A</option>
-                  <option>Company A</option>
-                  <option>Company C</option>
-                </select>
+                <Select options={this.state.locations} onChange={(event) => this.setState({ selectedLocation: event })}
+                  placeholder='Location' value={this.state.selectedLocation} />
               </div>
               <div className="col-md-4">
                 <div className="row">
@@ -159,51 +165,53 @@ export class VendorActivityToDo extends Component {
             </div>
           </div>
 
-          <div className="card-footer border-0">
-            <div className="d-flex  justify-content-between">
-              <label className="text-appprimary">Forms Status</label>
-              <div>
-                <input type="checkbox" className="btn-check" id="activitiesSaved" autoComplete="off" />
-                <label className="btn btn-outline-secondary" htmlFor="activitiesSaved">Activities Saved</label>
-              </div>
+          <div className="card-footer border-0 px-0">
+            <div className="d-flex justify-content-between">
+              <div className="d-flex align-items-center">
+                <div className="text-appprimary">Forms Status</div>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="activitiesSaved" autoComplete="off" />
+                  <label className="btn btn-outline-secondary" htmlFor="activitiesSaved">Activities Saved</label>
+                </div>
 
-              <div>
-                <input type="checkbox" className="btn-check" id="pending" autoComplete="off" />
-                <label className="btn btn-outline-warning" htmlFor="pending">Pending</label>
-              </div>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="pending" autoComplete="off" />
+                  <label className="btn btn-outline-warning" htmlFor="pending">Pending</label>
+                </div>
 
-              <div>
-                <input type="checkbox" className="btn-check" id="Overdue" autoComplete="off" />
-                <label className="btn btn-outline-danger" htmlFor="Overdue">Overdue</label>
-              </div>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="Overdue" autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="Overdue">Overdue</label>
+                </div>
 
-              <div>
-                <input type="checkbox" className="btn-check" id="Reject" autoComplete="off" />
-                <label className="btn btn-outline-danger" htmlFor="Reject">Reject</label>
-              </div>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="Reject" autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="Reject">Reject</label>
+                </div>
 
-              <div>
-                <input type="checkbox" className="btn-check" id="Submitted" autoComplete="off" />
-                <label className="btn btn-outline-danger" htmlFor="Submitted">Submitted</label>
-              </div>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="Submitted" autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="Submitted">Submitted</label>
+                </div>
 
-              <div>
-                <input type="checkbox" className="btn-check" id="Audited" autoComplete="off" />
-                <label className="btn btn-outline-danger" htmlFor="Audited">Audited</label>
+                <div className="mx-2">
+                  <input type="checkbox" className="btn-check" id="Audited" autoComplete="off" />
+                  <label className="btn btn-outline-danger" htmlFor="Audited">Audited</label>
+                </div>
               </div>
+              <div className="d-flex">
+                <div className="mx-2">
+                  <button className="btn btn-primary" onClick={this.handleShow.bind(this)}>
+                    Bulk Upload
+                  </button>
+                </div>
 
-              <div>
-                <button className="btn btn-primary" onClick={this.handleShow.bind(this)}>
-                  Bulk Upload
-                </button>
+                <div>
+                  <button type="submit" className="btn btn-danger">
+                    Submit To Auditor
+                  </button>
+                </div>
               </div>
-
-              <div>
-                <button type="submit" className="btn btn-danger">
-                  Submit To Auditor
-                </button>
-              </div>
-
             </div>
           </div>
         </form>
@@ -229,7 +237,6 @@ export class VendorActivityToDo extends Component {
           <tbody>
             {
               this.state.res.map(function (item, i) {
-                // const statusColor = this.computeStatusColor(item.status);
                 return (
                   <tr>
                     <td><input type="checkbox" /></td>
@@ -245,10 +252,31 @@ export class VendorActivityToDo extends Component {
                     <td className="text-danger">{item.auditRemarks}</td>
                     <td>
                       <div className="d-flex flex-row align-items-center">
-                        <FontAwesomeIcon icon={faDownload} className="mx-1 opacity-50" />
-                        <FontAwesomeIcon icon={faPencilSquare} className="mx-1 opacity-50" />
-                        <FontAwesomeIcon icon={faFloppyDisk} className="mx-1 opacity-50" />
-                        <FontAwesomeIcon icon={faEye} className="mx-1 opacity-50" />
+                        {/* Download */}
+                        <span className="me-1" style={{ zoom: 1.6, opacity: 0.5, cursor: "pointer" }}>
+                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.75 7.875V9.75H2.25V7.875H1V9.75C1 10.4375 1.5625 11 2.25 11H9.75C10.4375 11 11 10.4375 11 9.75V7.875H9.75Z" fill="#322C2D" />
+                            <path d="M9.125 5.375L8.24375 4.49375L6.625 6.10625L6.625 1L5.375 1L5.375 6.10625L3.75625 4.49375L2.875 5.375L6 8.5L9.125 5.375Z" fill="#322C2D" />
+                          </svg>
+                        </span>
+                        {/* Edit */}
+                        <span className="me-1" style={{ zoom: 1.6, opacity: 0.5, cursor: "pointer" }}>
+                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.5 10.8499C2.225 10.8499 1.9895 10.7521 1.7935 10.5564C1.5975 10.3607 1.49967 10.1252 1.5 9.8499V2.8499C1.5 2.5749 1.598 2.3394 1.794 2.1434C1.99 1.9474 2.22533 1.84957 2.5 1.8499H6.9625L5.9625 2.8499H2.5V9.8499H9.5V6.3749L10.5 5.3749V9.8499C10.5 10.1249 10.402 10.3604 10.206 10.5564C10.01 10.7524 9.77467 10.8502 9.5 10.8499H2.5ZM8.0875 2.1374L8.8 2.8374L5.5 6.1374V6.8499H6.2L9.5125 3.5374L10.225 4.2374L6.9125 7.5499C6.82083 7.64157 6.7145 7.71457 6.5935 7.7689C6.4725 7.82324 6.3455 7.85024 6.2125 7.8499H5C4.85833 7.8499 4.7395 7.8019 4.6435 7.7059C4.5475 7.6099 4.49967 7.49124 4.5 7.3499V6.1374C4.5 6.00407 4.525 5.8769 4.575 5.7559C4.625 5.6349 4.69583 5.52874 4.7875 5.4374L8.0875 2.1374ZM10.225 4.2374L8.0875 2.1374L9.3375 0.887402C9.5375 0.687402 9.77717 0.587402 10.0565 0.587402C10.3358 0.587402 10.5712 0.687402 10.7625 0.887402L11.4625 1.5999C11.6542 1.79157 11.75 2.0249 11.75 2.2999C11.75 2.5749 11.6542 2.80824 11.4625 2.9999L10.225 4.2374Z" fill="#322C2D" />
+                          </svg>
+                        </span>
+                        {/* Save */}
+                        <span className="me-1" style={{ zoom: 1.6, opacity: 0.5, cursor: "pointer" }}>
+                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.5 3.5V10C10.5 10.275 10.4022 10.5105 10.2065 10.7065C10.0105 10.9022 9.775 11 9.5 11H2.5C2.225 11 1.9895 10.9022 1.7935 10.7065C1.59783 10.5105 1.5 10.275 1.5 10L1.5 2C1.5 1.725 1.59783 1.4895 1.7935 1.2935C1.9895 1.09783 2.225 1 2.5 1H8.5L10.5 3.5ZM9.5 3.925L8.075 2H2.5V10H9.5V3.925ZM6 9C6.41667 9 6.77083 8.85417 7.0625 8.5625C7.35417 8.27083 7.5 7.91667 7.5 7.5C7.5 7.08333 7.35417 6.72917 7.0625 6.4375C6.77083 6.14583 6.41667 6 6 6C5.58333 6 5.22917 6.14583 4.9375 6.4375C4.64583 6.72917 4.5 7.08333 4.5 7.5C4.5 7.91667 4.64583 8.27083 4.9375 8.5625C5.22917 8.85417 5.58333 9 6 9ZM3 4.575H7.5V3.5H3V4.575ZM2.5 3.925V10V2V3.925Z" fill="#322C2D" />
+                          </svg>
+                        </span>
+                        {/* View */}
+                        <span style={{ zoom: 1.6, opacity: 0.5, cursor: "pointer" }}>
+                          <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6 4.5C6.39782 4.5 6.77936 4.65804 7.06066 4.93934C7.34196 5.22064 7.5 5.60218 7.5 6C7.5 6.39782 7.34196 6.77936 7.06066 7.06066C6.77936 7.34196 6.39782 7.5 6 7.5C5.60218 7.5 5.22064 7.34196 4.93934 7.06066C4.65804 6.77936 4.5 6.39782 4.5 6C4.5 5.60218 4.65804 5.22064 4.93934 4.93934C5.22064 4.65804 5.60218 4.5 6 4.5ZM6 2.25C8.5 2.25 10.635 3.805 11.5 6C10.635 8.195 8.5 9.75 6 9.75C3.5 9.75 1.365 8.195 0.5 6C1.365 3.805 3.5 2.25 6 2.25ZM1.59 6C1.99413 6.82515 2.62165 7.52037 3.40124 8.00663C4.18083 8.49288 5.0812 8.75066 6 8.75066C6.9188 8.75066 7.81917 8.49288 8.59876 8.00663C9.37835 7.52037 10.0059 6.82515 10.41 6C10.0059 5.17485 9.37835 4.47963 8.59876 3.99337C7.81917 3.50712 6.9188 3.24934 6 3.24934C5.0812 3.24934 4.18083 3.50712 3.40124 3.99337C2.62165 4.47963 1.99413 5.17485 1.59 6Z" fill="#322C2D" />
+                          </svg>
+                        </span>
                       </div>
                     </td>
                   </tr>
